fe35b51fe2bbb19a52554e28f3de2f1703dbea2d
Alex Shalov <shalov-aleksey@mail.ru>
2026-01-06 23:55:15 +0300

    add3 06.01.2026

--- Incapsulacia/Polymorphism/Main.java
+++ Incapsulacia/Polymorphism/Main.java
@@ -9,20 +9,5 @@ public class Main {
         System.out.println("Площадь круга: " + circle.getArea());
         System.out.println("Площадь квадрата: " + square.getArea());
         System.out.println("Площадь прямоугольника: " + rectangle.getArea());
-
-        System.out.println("=======================");
-
-
-        Shape[] shapes = {
-                new Circle(0, 0, 5),
-                new Circle(2, 3, 3),
-                new Square(1, 1, 4),
-                new Circle(10, 10, 7),
-                new Square(5, 5, 6),
-                new Circle(8, 8, 2.5)
-        };
-        double totalArea1 = AreaCalculator.sumAllAreas(shapes);
-        System.out.println("Общая площадь всех фигур: " +
-                String.format("%.2f", totalArea1));
     }
 }
